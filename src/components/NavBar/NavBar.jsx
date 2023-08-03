87527115f896348e17037f253131841a73a67ad5
Gab <gab@gab.art.br>
2023-08-03 12:49:02 -0300

    :recycle: refactor: improve title handling and prop usage

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,44 +3,39 @@ import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { NavBarContainer, StyledLink } from './NavBarStyles';
 
+function getTitle(pathname) {
+  const titles = {
+    '/': 'Home',
+    '/pessoa-usuaria': 'Pessoa Usuária',
+    '/profissional': 'Profissional',
+  };
+
+  return titles[pathname] || 'Página não encontrada';
+}
+
 function NavBar({ variant }) {
   const location = useLocation();
 
   useEffect(() => {
-    let title;
-
-    switch (location.pathname) {
-      case '/':
-        title = 'Home';
-        break;
-      case '/pessoa-usuaria':
-        title = 'Pessoa Usuária';
-        break;
-      case '/profissional':
-        title = 'Profissional';
-        break;
-      default:
-        title = 'Página não encontrada';
-    }
-
+    const title = getTitle(location.pathname);
     document.title = `${title} | Desafio Lacrei`;
   }, [location]);
 
   return (
-    <NavBarContainer>
-      <StyledLink active={location.pathname === '/'} variant={variant} to='/'>
+    <NavBarContainer $variant={variant}>
+      <StyledLink $active={location.pathname === '/'} $variant={variant} to='/'>
         Home
       </StyledLink>
       <StyledLink
-        active={location.pathname === '/pessoa-usuaria'}
-        variant={variant}
+        $active={location.pathname === '/pessoa-usuaria'}
+        $variant={variant}
         to='/pessoa-usuaria'
       >
         Pessoa Usuária
       </StyledLink>
       <StyledLink
-        active={location.pathname === '/profissional'}
-        variant={variant}
+        $active={location.pathname === '/profissional'}
+        $variant={variant}
         to='/profissional'
       >
         Profissional
--- src/components/NavBar/NavBarStyles.js
+++ src/components/NavBar/NavBarStyles.js
@@ -5,16 +5,26 @@ export const NavBarContainer = styled.nav`
   display: flex;
   gap: 2.5rem;
   justify-content: space-between;
+
+  @media (max-width: 520px) {
+    flex-direction: ${(props) =>
+      props.$variant === 'header' ? 'row' : 'column'};
+    gap: ${(props) => (props.$variant === 'header' ? '1rem' : '0.25rem')};
+  }
+
+  @media (min-width: 521px) and (max-width: 1040px) {
+    gap: ${(props) => (props.$variant === 'header' ? '2rem' : '2.5rem')};
+  }
 `;
 
 export const StyledLink = styled(Link)`
   font-size: 1rem;
   font-weight: ${(props) =>
-    props.active || props.variant === 'header' ? '700' : '400'};
+    props.$active || props.$variant === 'header' ? '700' : '400'};
   text-decoration: none;
   transition: all 0.2s ease 0s;
   color: ${(props) =>
-    props.active && props.variant === 'header'
+    props.$active && props.$variant === 'header'
       ? 'rgb(1, 135, 98)'
       : 'rgb(31, 31, 31)'};
   align-items: center;
@@ -22,5 +32,9 @@ export const StyledLink = styled(Link)`
 
   &:hover {
     opacity: 0.7;
+  }
+
+  @media (max-width: 520px) {
+    font-size: 0.875rem;
   }
 `;
